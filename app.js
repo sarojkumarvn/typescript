ef2744770ed530920228966eeb0be90b637abf03
sarojkumar <sarojojha2457@gmail.com>
2025-06-05 23:33:27 +0530

    Done with the interface

--- app.js
+++ app.js
@@ -4,9 +4,35 @@
 // }
 // console.log(calculation(num1,2));
 // checking 2
-function getTotal(numbers) {
+function calculatesum(numbers) {
     return numbers.reduce(function (acc, item) {
         return acc + item;
     }, 0);
 }
-console.log(getTotal([3, 2, 1, 54, 87, 963]));
+console.log(calculatesum([3, 2, 1, 54, 87, 963]));
+var user = {
+    // we have to write type just after giving the column
+    name: "Saroj kumar ojha",
+    age: 20,
+    location: " Odisha",
+};
+function login(userData) {
+    return userData;
+}
+console.log(login(user));
+var transaction1 = {
+    payerAccountNumber: 1274,
+    payeeAccountNumber: 2148785,
+};
+var transaction2 = {
+    payerAccountNumber: 1274,
+    payeeAccountNumber: 2148785,
+};
+var bankAccount = {
+    accountNumber: 1234,
+    accountHolder: "Saroj kumar ojha",
+    balance: 12125454,
+    isActive: true,
+    transactions: [transaction1, transaction2],
+};
+console.log(bankAccount);
--- app.ts
+++ app.ts
@@ -12,7 +12,7 @@ function calculatesum(numbers: number[]) {
   }, 0);
 }
 
-console.log(calculatesum([3, 2, 1, 54, 87, 963]));
+// console.log(calculatesum([3, 2, 1, 54, 87, 963]));
 
 //checking3
 
@@ -31,11 +31,111 @@ const user: User = {
 
 function login(userData: User): User {
   return userData;
-
 }
 
 console.log(login(user));
 
+//INTERFACES - shape of the object
+interface Transaction {
+  payerAccountNumber: number;
+  payeeAccountNumber: number;
+}
 
-//INTERFACES
+interface BankAccount {
+  accountNumber: number;
+  accountHolder: string;
+  balance: number;
+  isActive: boolean;
+  transactions: Transaction[];
+}
 
+const transaction1: Transaction = {
+  payerAccountNumber: 1274,
+  payeeAccountNumber: 2148785,
+};
+const transaction2: Transaction = {
+  payerAccountNumber: 1274,
+  payeeAccountNumber: 2148785,
+};
+
+const bankAccount: BankAccount = {
+  accountNumber: 1234,
+  accountHolder: "Saroj kumar ojha",
+  balance: 12125454,
+  isActive: true,
+  transactions: [transaction1, transaction2],
+};
+
+// console.log(bankAccount);
+
+// EXTENT TWO INTERFACES
+interface Book {
+  title: string;
+  price: number;
+  rating?: number; //optional
+}
+
+const book: Book = {
+  title: "Rich Dad and poor Dad",
+  price: 300,
+  rating: 4.5,
+  size: 500,
+  format: "PDF",
+};
+
+interface EBook extends Book {
+  fileSize: number;
+  format: string;
+}
+
+const eBook: EBook = {
+  title: "Rich Dad and poor Dad",
+  price: 300,
+  fileSize: 500,
+size: 500,
+  format: "PDF",
+};
+
+interface AudioBook extends EBook {
+  // title: string;
+  // price: number;
+  duration: number;
+  // format: string;
+  // fileSize: number;
+}
+
+const audioBook: AudioBook = {
+  title: "Rich Dad and poor Dad",
+  price: 300,
+  size: 500,
+  duration: 500,
+  format: "MP3",
+  fileSize: 500,
+};
+
+
+
+// TO MERGE TWO INTERFACES
+interface Book {
+  title: string;
+  price: number;
+  
+}
+
+interface Book {
+  size: number;
+  format: string;
+}
+
+// same interfaces get merged with each other
+// if two interfaces have the same name then ts will merge themwith each other
+
+
+
+const NewBook : Book = {
+  title: "Rich Dad and poor Dad",
+  price: 300,
+  size: 500,
+  format: "PDF",
+  
+}
