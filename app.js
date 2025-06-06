ed614262dad0944bcb6df050568a56fa804cd6e2
sarojkumar <sarojojha2457@gmail.com>
2025-06-06 18:32:47 +0530

    Reachyed Generics

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ function calculatesum(numbers) {
         return acc + item;
     }, 0);
 }
-console.log(calculatesum([3, 2, 1, 54, 87, 963]));
 var user = {
     // we have to write type just after giving the column
     name: "Saroj kumar ojha",
@@ -19,7 +18,6 @@ var user = {
 function login(userData) {
     return userData;
 }
-console.log(login(user));
 var transaction1 = {
     payerAccountNumber: 1274,
     payeeAccountNumber: 2148785,
@@ -35,4 +33,62 @@ var bankAccount = {
     isActive: true,
     transactions: [transaction1, transaction2],
 };
-console.log(bankAccount);
+var book = {
+    title: "Rich Dad and poor Dad",
+    price: 300,
+    rating: 4.5,
+    size: 500,
+    format: "PDF",
+};
+var eBook = {
+    title: "Rich Dad and poor Dad",
+    price: 300,
+    fileSize: 500,
+    size: 500,
+    format: "PDF",
+};
+var audioBook = {
+    title: "Rich Dad and poor Dad",
+    price: 300,
+    size: 500,
+    duration: 500,
+    format: "MP3",
+    fileSize: 500,
+};
+// same interfaces get merged with each other
+// if two interfaces have the same name then ts will merge themwith each other
+var NewBook = {
+    title: "Rich Dad and poor Dad",
+    price: 300,
+    size: 500,
+    format: "PDF",
+};
+// Narrowing
+function PrintId(id) {
+    // we declared the type of id which is either number or string
+    if (typeof id === "string")
+        console.log(id.toUpperCase());
+    else
+        console.log(id);
+}
+console.log(PrintId(123)); // number
+console.log(PrintId("saroj")); // string
+function getFirstThree(x) {
+    return x.slice(0, 3);
+}
+console.log(getFirstThree([1, 2, 3, 4, 5]));
+//GENERICS - it is generalized code , used for making reusable codeeven after we change the type
+function LogString(args) {
+    console.log(args.toUpperCase());
+    return args;
+}
+function LogNumber(args) {
+    console.log(args);
+    return args;
+}
+function LogArray(args) {
+    console.log(args);
+    return args;
+}
+LogString("Saroj");
+LogArray([1, 2, 3, 4, 5]);
--- app.ts
+++ app.ts
@@ -33,7 +33,7 @@ function login(userData: User): User {
   return userData;
 }
 
-console.log(login(user));
+// console.log(login(user));
 
 //INTERFACES - shape of the object
 interface Transaction {
@@ -92,7 +92,7 @@ const eBook: EBook = {
   title: "Rich Dad and poor Dad",
   price: 300,
   fileSize: 500,
-size: 500,
+  size: 500,
   format: "PDF",
 };
 
@@ -113,13 +113,10 @@ const audioBook: AudioBook = {
   fileSize: 500,
 };
 
-
-
 // TO MERGE TWO INTERFACES
 interface Book {
   title: string;
   price: number;
-  
 }
 
 interface Book {
@@ -130,12 +127,67 @@ interface Book {
 // same interfaces get merged with each other
 // if two interfaces have the same name then ts will merge themwith each other
 
-
-
-const NewBook : Book = {
+const NewBook: Book = {
   title: "Rich Dad and poor Dad",
   price: 300,
   size: 500,
   format: "PDF",
-  
+};
+
+//UNIONS
+
+type ID = number | string;
+
+// Narrowing
+function PrintId(id: ID) {
+  // we declared the type of id which is either number or string
+  if (typeof id === "string") console.log(id.toUpperCase());
+  else console.log(id);
+}
+
+console.log(PrintId(123)); // number
+console.log(PrintId("saroj")); // string
+
+function getFirstThree(x: string | number[]) {
+  return x.slice(0, 3);
+}
+
+console.log(getFirstThree([1, 2, 3, 4, 5]));
+
+//GENERICS - it is generalized code , used for making reusable codeeven after we change the type
+
+function LogString(args: string) {
+  console.log(args.toUpperCase());
+  return args;
+}
+function LogNumber(args: number) {
+  console.log(args);
+  return args;
+}
+
+function LogArray(args: any) {
+  // we are ignoring type
+  console.log(args);
+  return args;
+}
+
+function LogAnything<T>(args: T): T {
+  console.log(args);
+  return args;
+}
+// in this any case we will use generics , that is like you can give any type of data in
+// to that function but it wont change the type security
+// this T will automatically take the type of the data that we are passing through the function
+
+// LogString("Saroj");
+// LogArray([1,2,3,4,5]);
+// LogAnything([1,2.1,4,5]);
+
+// ANOTHER EXAMPLE FOR GENERIC
+interface HasAge {
+  age: number;
+}
+
+function getOldest(people: HasAge[]): HasAge {
+  return people.sort((a, b) => a.age - b.age)[people.length - 1];
 }
